bd6ef0354f2a29dd5d153c55c23a0692dd92ff04
MArndt26 <mitchaarndt@gmail.com>
2021-06-12 12:18:57 -0700

    updated Board and started working on ship.js

--- src/game/Board.js
+++ src/game/Board.js
@@ -1,13 +1,15 @@
+/**
+ * Game Board where integers are used to represent the state of the game
+ *   0: is the initial "untouched" state of the board space
+ *   1: counts as a miss
+ *   2: counts as a hit
+ */
 class Board {
-  constructor(size = 8) {
+  constructor(size = 8, ships) {
     this.size = size;
     this.grid = Array.from(Array(size), () => new Array(size));
-
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
-        this.grid[i][j] = 0;
-      }
-    }
+    this.ships = ships;
+    this.clear();
     this.printBoard();
   }
 
@@ -24,6 +26,30 @@ class Board {
         process.stdout.write(`${this.grid[i][j]}, `);
       }
       process.stdout.write("\n");
+    }
+  }
+
+  /**
+   * Function used to attempt to mark a spot on the game board
+   * @param row letter corresponding to row
+   * @param col 1 indexed column
+   */
+  mark(row, col) {
+    row = row.toLowerCase().charCodeAt(0) - 97;
+    col--;
+    if (this.grid[col][row] === 1) {
+      return false;
+    } else {
+      this.grid[col][row] = 1;
+      return true;
+    }
+  }
+
+  clear() {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        this.grid[i][j] = 0;
+      }
     }
   }
 }
