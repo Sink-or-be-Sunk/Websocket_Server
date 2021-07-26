0c65f65d44c482511b0ce64d6998202edd9ba8e8
MArndt26 <mitchaarndt@gmail.com>
2021-07-25 18:19:38 -0700

    made including header conditional

--- src/server/routes/game.ts
+++ src/server/routes/game.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.render("game/index");
+	res.render("game/index", { header: false });
 });
 
 export default router;
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.render("index");
+	res.render("index", { header: true });
 });
 
 export default router;
