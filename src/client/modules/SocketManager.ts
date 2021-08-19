460c36fca91c099cd04de60ec564bfc1cbbf8af5
Mitch <mitchaarndt@gmail.com>
2021-08-18 18:43:31 -0700

    added socket manager to communicate with websocket server

--- src/client/modules/SocketManager.ts
+++ src/client/modules/SocketManager.ts
@@ -1,8 +1,19 @@
+import { v4 as uuidv4 } from "uuid";
+import WSClientMessage from "../../server/models/WSClientMessage";
+
 export default class SocketManager {
 	socket: WebSocket;
+	id: string;
 
 	constructor() {
 		this.socket = new WebSocket("ws://sink-or-be-sunk.herokuapp.com/");
+		const cachedID = window.localStorage.getItem("id");
+		if (cachedID) {
+			this.id = cachedID;
+		} else {
+			this.id = uuidv4();
+			window.localStorage.setItem("id", this.id);
+		}
 
 		this.socket.onopen = function (event) {
 			console.log(event);
@@ -11,7 +22,11 @@ export default class SocketManager {
 		this.socket.onmessage = this.handleServerMessage;
 
 		this.socket.onopen = (event) => {
-			this.socket.send("hello server!");
+			const newGame = {
+				req: WSClientMessage.REQ_TYPE.NEW_GAME,
+				id: this.id,
+			};
+			this.socket.send(JSON.stringify(newGame));
 		};
 	}
 
