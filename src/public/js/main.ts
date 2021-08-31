580d37c639e8c7da0a01acde48c036cfa44a8301
MArndt26 <mitchaarndt@gmail.com>
2021-08-31 10:04:33 -0400

    left off trying to get ts loading to work

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,4 +1,13 @@
-$(document).ready(function() {
-  // Place JavaScript code here...
+import SomeClass from "./modules/something";
+import * as THREE from "three";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
+const str = new SomeClass("string");
 
-});
+const vect = new THREE.Vector2();
+
+const loader = new GLTFLoader();
+loader.load("something", (gltf)=> {
+  console.log("loaded"), undefined, (err: ErrorEvent)=> {
+    console.error(err);
+  } 
+})
---
+++ src/public/js/modules/something.ts
@@ -0,0 +1,6 @@
+export default class SomeClass {
+    str: string
+    constructor(str: string) {
+        this.str = str;
+    }
+}
