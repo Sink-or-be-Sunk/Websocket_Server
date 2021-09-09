b0466a61e3f686ee8a8f129a1b68060c9cef6037
MArndt26 <mitchaarndt@gmail.com>
2021-09-09 10:09:47 -0400

    left off working on input.manager.test

--- test/registration/input.manager.test.ts
+++ test/registration/input.manager.test.ts
@@ -22,4 +22,13 @@ describe("Validate Registration Client Messages", () => {
         const resp = manager.handleReq(msg);
         expect(resp).toEqual(ServerMessenger.REGISTER_PENDING)
     });
+
+    it("Accepts Client Confirm Message", () => {
+        const register = { type: REGISTER_TYPE.CONFIRM, ssid: "wifi" };
+        const obj = { req: REQ_TYPE.REGISTER, id: "one", data: register };
+        const msg = new WSClientMessage(JSON.stringify(obj));
+        const resp = manager.handleReq(msg);
+        expect(resp).toEqual(ServerMessenger.REGISTER_SUCCESS)
+    });
+
 });
