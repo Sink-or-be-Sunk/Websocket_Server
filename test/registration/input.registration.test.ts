7f71d7886f67b5ccaf4b411537064805f763299c
Mitch <mitchaarndt@gmail.com>
2021-09-14 16:39:03 -0400

    all test cases working

--- test/registration/input.registration.test.ts
+++ test/registration/input.registration.test.ts
@@ -7,21 +7,21 @@ describe("Handle Registration Requests", () => {
 	it("Accepts Init Message", () => {
 		const obj = { type: REGISTER_TYPE.ENQUEUE, ssid: "wifi" };
 		const msg = new RegisterRequest(obj);
-		expect(msg).toEqual({ type: obj.type, ssid: obj.ssid });
+		expect(msg).toEqual({ type: obj.type, ssid: obj.ssid, data: "" });
 		expect(msg.isValid()).toBe(true);
 	});
 
 	it("Accepts Confirm Message", () => {
 		const obj = { type: REGISTER_TYPE.CONFIRM, ssid: "wifi" };
 		const msg = new RegisterRequest(obj);
-		expect(msg).toEqual({ type: obj.type, ssid: obj.ssid });
+		expect(msg).toEqual({ type: obj.type, ssid: obj.ssid, data: "" });
 		expect(msg.isValid()).toBe(true);
 	});
 
 	it("Rejects Message with bad register type", () => {
 		const obj = { type: "some random type", ssid: "wifi" };
 		const msg = new RegisterRequest(obj);
-		expect(msg).toEqual({ type: REGISTER_TYPE.INVALID, ssid: "" });
+		expect(msg).toEqual({ type: REGISTER_TYPE.INVALID, ssid: "", data:"" });
 		expect(msg.isValid()).toBe(false);
 	});
 
@@ -31,6 +31,7 @@ describe("Handle Registration Requests", () => {
 		expect(msg).toEqual({
 			type: REGISTER_TYPE.BAD_FORMAT,
 			ssid: "",
+			data: ""
 		});
 		expect(msg.isValid()).toBe(false);
 	});
