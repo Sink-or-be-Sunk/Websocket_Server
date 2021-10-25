f6e33c4ca51f2cb418ffd927d408618fa5cb5a34
MArndt26 <mitchaarndt@gmail.com>
2021-10-24 23:35:05 -0400

    working web game

--- src/public/js/Game2D.ts
+++
@@ -1,9 +0,0 @@
-class Game2D {
-	socket: GameSocket;
-	isBasicMode: boolean;
-
-	constructor(socket: GameSocket) {
-		this.socket = socket;
-		this.isBasicMode = false;
-	}
-}
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -15,7 +15,7 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const MONGODB_URI = true
+export const MONGODB_URI = prod
 	? process.env["MONGODB_URI"]
 	: process.env["MONGODB_URI_LOCAL"];
 
