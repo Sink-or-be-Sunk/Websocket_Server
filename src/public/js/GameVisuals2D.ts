41e4cb82e25ff265a16c1054cfa49174309157de
Mitch Win Laptop <mitchaarndt@gmail.com>
2021-11-28 09:08:04 -0700

    before nuking game2d.scss

--- src/public/js/GameVisuals2D.ts
+++ src/public/js/GameVisuals2D.ts
@@ -1,34 +1,46 @@
-interface sizeIF {
-	w: number;
-	h: number;
-	r: number;
-	c: number;
-	snap: number;
-}
+// import { Console } from "console";
 
-class GameVisuals2D {
-	private attackBoard: JQuery<HTMLElement>;
-	private positionBoard: JQuery<HTMLElement>;
+// interface sizeIF {
+// 	w: number;
+// 	h: number;
+// 	r: number;
+// 	c: number;
+// 	snap: number;
+// }
 
-	private config: sizeIF;
+// class GameVisuals2D {
+// 	private attackBoard: JQuery<HTMLElement>;
+// 	private positionBoard: JQuery<HTMLElement>;
 
-	private Draggable: any;
-	constructor(Draggable) {
-		this.Draggable = Draggable;
-		this.attackBoard = $("#attack-board");
-		this.positionBoard = $("#position-board");
-		this.config = {
-			w: 50,
-			h: 50,
-			r: 8,
-			c: 8,
-			snap: 50,
-		};
+// 	private config: sizeIF;
 
-		this.Draggable.create("boat2", {
-			bounds: "container",
-			onDrag: "onDrag",
-			onClick: "rotateShip",
-		});
-	}
-}
+// 	private Draggable: any;
+// 	private TweenLite: any;
+// 	constructor(Draggable: any, TweenLite: any) {
+// 		console.log("Game Visuals 2D");
+// 		this.Draggable = Draggable;
+// 		this.TweenLite = TweenLite;
+// 		this.attackBoard = $("#attack-board");
+// 		this.positionBoard = $("#position-board");
+// 		this.config = {
+// 			w: 50,
+// 			h: 50,
+// 			r: 8,
+// 			c: 8,
+// 			snap: 50,
+// 		};
+
+// 		this.Draggable.create("boat2", {
+// 			bounds: "container",
+// 			onDrag: "onDrag",
+// 			onClick: "rotateShip",
+// 		});
+// 	}
+// 	public onDrag() {
+// 		this.TweenLite.to(this.target, 0.5, {
+// 			x: Math.round(this.x / this.config.snap) * this.config.snap,
+// 			y: Math.round(this.y / this.config.snap) * this.config.snap,
+// 			ease: Back.easeOut.config(2),
+// 		});
+// 	}
+// }
